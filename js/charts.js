cf948ffe2ccbbdfb19f7210ea1221cbd5a636a6a
Cormac Wilson <cw2063@hw.ac.uk>
2026-02-01 00:01:18 +0000

    Amended code for login.js and charts.js, also added icons to navbar and login page fields.

--- js/charts.js
+++ js/charts.js
@@ -1,91 +1,259 @@
-function chart_tripByUsage() {
-    const ctx = document.getElementById('tripsByUsage');
+// The following four functions are used for fetching and displaying charts for transport information.
+
+function chart_trips_by_usage() {
+    const ctx = document.getElementById('trips_by_usage');
 
     new Chart(ctx, {
-        type: 'bar',
+        type: "pie",
         data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: xValues,
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            borderWidth: 1
-        }]
+            backgroundColor: barColors,
+            data: yValues
+            }]
         },
         options: {
-        scales: {
-            y: {
-            beginAtZero: true
-            }
-        }
-        }
+            title: {
+            display: true,
+            text: "Trips by usage"
+            },
+            plugins: {
+                legend: {
+                    labels: {
+                        font: {
+                            family: 'Inter, sans-serif',
+                            style: 'normal',
+                            weight: 'bold',
+                        },
+                    },
+                },
+            },
+        },
     });
 }
 
-function chart_tripByRegistration() {
-    const ctx = document.getElementById('tripsByRegistration');
+function chart_trips_by_registration() {
+    const ctx = document.getElementById('trips_by_registration');
 
     new Chart(ctx, {
-        type: 'bar',
+        type: "pie",
         data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: xValues,
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            borderWidth: 1
-        }]
+            backgroundColor: barColors,
+            data: yValues
+            }]
         },
         options: {
-        scales: {
-            y: {
-            beginAtZero: true
-            }
-        }
-        }
+            title: {
+            display: true,
+            text: "Trips by registration"
+            },
+            plugins: {
+                legend: {
+                    labels: {
+                        font: {
+                            family: 'Inter, sans-serif',
+                            style: 'normal',
+                            weight: 'bold',
+                        },
+                    },
+                },
+            },
+        },
     });
 }
 
-function chart_tripByTypeOfVehicle() {
-    const ctx = document.getElementById('tripsByTypeOfVehicle');
+function chart_trips_by_type_of_vehicle() {
+    const ctx = document.getElementById('trips_by_type_of_vehicle');
 
     new Chart(ctx, {
-        type: 'bar',
+        type: "pie",
         data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: xValues,
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            borderWidth: 1
-        }]
+            backgroundColor: barColors,
+            data: yValues
+            }]
         },
         options: {
-        scales: {
-            y: {
-            beginAtZero: true
-            }
-        }
-        }
+            title: {
+            display: true,
+            text: "Trips by type of vehicle"
+            },
+            plugins: {
+                legend: {
+                    labels: {
+                        font: {
+                            family: 'Inter, sans-serif',
+                            style: 'normal',
+                            weight: 'bold',
+                        },
+                    },
+                },
+            },
+        },
     });
 }
 
-function chart_tripByDestination() {
-    const ctx = document.getElementById('tripsByDestination');
+function chart_trips_by_destination() {
+    const ctx = document.getElementById('trips_by_destination');
 
     new Chart(ctx, {
-        type: 'bar',
+        type: "pie",
         data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: xValues,
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            borderWidth: 1
-        }]
+            backgroundColor: barColors,
+            data: yValues
+            }]
         },
         options: {
-        scales: {
-            y: {
-            beginAtZero: true
-            }
-        }
-        }
+            title: {
+            display: true,
+            text: "Trips by destination"
+            },
+            plugins: {
+                legend: {
+                    labels: {
+                        font: {
+                            family: 'Inter, sans-serif',
+                            style: 'normal',
+                            weight: 'bold',
+                        },
+                    },
+                },
+            },
+        },
+    });
+}
+
+// The following four functions are used for fetching and displaying charts for attendance information.
+
+function chart_average_attendance_for_beginner_lessons() {
+    const ctx = document.getElementById('average_attendance_for_beginner_lessons');
+
+    new Chart(ctx, {
+        type: "pie",
+        data: {
+        labels: xValues,
+        datasets: [{
+            backgroundColor: barColors,
+            data: yValues
+            }]
+        },
+        options: {
+            title: {
+            display: true,
+            text: "Average attendance for beginner lessons"
+            },
+            plugins: {
+                legend: {
+                    labels: {
+                        font: {
+                            family: 'Inter, sans-serif',
+                            style: 'normal',
+                            weight: 'bold',
+                        },
+                    },
+                },
+            },
+        },
+    });
+}
+
+function chart_average_attendance_for_intermediate_lessons() {
+    const ctx = document.getElementById('average_attendance_for_intermediate_lessons');
+
+    new Chart(ctx, {
+        type: "pie",
+        data: {
+        labels: xValues,
+        datasets: [{
+            backgroundColor: barColors,
+            data: yValues
+            }]
+        },
+        options: {
+            title: {
+            display: true,
+            text: "Average attendance for intermediate lessons"
+            },
+            plugins: {
+                legend: {
+                    labels: {
+                        font: {
+                            family: 'Inter, sans-serif',
+                            style: 'normal',
+                            weight: 'bold',
+                        },
+                    },
+                },
+            },
+        },
+    });
+}
+
+function chart_average_attendance_for_training_sessions() {
+    const ctx = document.getElementById('average_attendance_for_training_sessions');
+
+    new Chart(ctx, {
+        type: "pie",
+        data: {
+        labels: xValues,
+        datasets: [{
+            backgroundColor: barColors,
+            data: yValues
+            }]
+        },
+        options: {
+            title: {
+            display: true,
+            text: "Average attendance for training sessions"
+            },
+            plugins: {
+                legend: {
+                    labels: {
+                        font: {
+                            family: 'Inter, sans-serif',
+                            style: 'normal',
+                            weight: 'bold',
+                        },
+                    },
+                },
+            },
+        },
+    });
+}
+
+function chart_average_attendance_for_trips() {
+    const ctx = document.getElementById('average_attendance_for_trips');
+
+    new Chart(ctx, {
+        type: "pie",
+        data: {
+        labels: xValues,
+        datasets: [{
+            backgroundColor: barColors,
+            data: yValues
+            }]
+        },
+        options: {
+            title: {
+            display: true,
+            text: "Average attendance for trips"
+            },
+            plugins: {
+                legend: {
+                    labels: {
+                        font: {
+                            family: 'Inter, sans-serif',
+                            style: 'normal',
+                            weight: 'bold',
+                        },
+                    },
+                },
+            },
+        },
     });
 }
--- js/login.js
+++ js/login.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("login_form").addEventListener("submit", function () {
         const btn = document.getElementById("submit");
+        const icon = btn.querySelector(".input_icon");
         btn.classList.add("loading");
         btn.disabled = true;
+        icon.style.display = "none";
     });
 });
